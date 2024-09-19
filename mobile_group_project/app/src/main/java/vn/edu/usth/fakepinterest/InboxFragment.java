8faad48301f5c9c090811f564cce29de8e1aa271
catcandance2004 <catcandance2004@gmail.com>
2024-09-19 13:52:53 +0700

    Notification update

--- mobile_group_project/app/src/main/java/vn/edu/usth/fakepinterest/InboxFragment.java
+++ mobile_group_project/app/src/main/java/vn/edu/usth/fakepinterest/InboxFragment.java
@@ -1,20 +1,16 @@
 package vn.edu.usth.fakepinterest;
 
 import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import androidx.fragment.app.Fragment;
+
 
 public class InboxFragment extends Fragment {
 
-    @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_inbox, container, false); // Create fragment_inbox.xml
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_inbox, container, false);
     }
 }
