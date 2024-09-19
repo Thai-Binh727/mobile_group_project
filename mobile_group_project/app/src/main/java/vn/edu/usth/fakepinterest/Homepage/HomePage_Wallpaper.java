ea39c2c13b5904e428c98543bcf450bebe502555
Thai-Binh727 <binhnt0609@gmail.com>
2024-09-19 10:11:30 +0700

    updated function for homepage

--- mobile_group_project/app/src/main/java/vn/edu/usth/fakepinterest/Homepage/HomePage_Wallpaper.java
+++ mobile_group_project/app/src/main/java/vn/edu/usth/fakepinterest/Homepage/HomePage_Wallpaper.java
@@ -3,19 +3,32 @@ package vn.edu.usth.fakepinterest.Homepage;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import vn.edu.usth.fakepinterest.R;
 
 public class HomePage_Wallpaper extends Fragment {
+    private Button button_1;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_homepage_wallpaper, container, false);
+        View view = inflater.inflate(R.layout.fragment_homepage_wallpaper, container, false);
+            button_1 = view.findViewById(R.id.button_wallpaper_1);
+            button_1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Fragment clicked = new clicked_on_image();
+                    FragmentTransaction fm = getActivity().getSupportFragmentManager().beginTransaction();
+                    fm.replace(R.id.main, clicked).commit();
+                }
+            });
+        return view;
     }
 }
--- mobile_group_project/app/src/main/java/vn/edu/usth/fakepinterest/Homepage/clicked_on_image.java
+++ mobile_group_project/app/src/main/java/vn/edu/usth/fakepinterest/Homepage/clicked_on_image.java
@@ -1,21 +1,39 @@
 package vn.edu.usth.fakepinterest.Homepage;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 
+import vn.edu.usth.fakepinterest.MainActivity;
 import vn.edu.usth.fakepinterest.R;
 
 public class clicked_on_image extends Fragment {
+
+    private ImageButton button_back_homepage;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_clicked_on_image, container, false);
+        View view = inflater.inflate(R.layout.fragment_clicked_on_image, container, false);
+
+        button_back_homepage = view.findViewById(R.id.button_main);
+        button_back_homepage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Create an Intent to navigate to MainActivity
+                Intent intent = new Intent(getActivity(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        return view;
     }
 }
